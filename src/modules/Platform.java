fcf5d8dadfb833291c8d28e1776b32fea496e3a1
Zishan Khan <sendtozishan@gmail.com>
2021-08-23 23:49:15 +0530

    Initial commit

---
+++ src/modules/Platform.java
@@ -0,0 +1,33 @@
+package modules;
+
+public class Platform {
+    String name;
+    String vertical;
+    String status;
+
+    Platform(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVertical() {
+        return vertical;
+    }
+
+    public void setVertical(String vertical) {
+        this.vertical = vertical;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
